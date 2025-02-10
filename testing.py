e199a099aa5ea4c2bb1ff9c0a50bdccdedbc2f94
Srinivas-jatothu <cs21b023@iittp.ac.in>
2025-02-10 22:29:18 +0530

    Testing commit

---
+++ testing.py
@@ -0,0 +1,4 @@
+#In this project we will try to crate a model which will take the input as video and generates the o.p as images. Here generated images are nothing but summazied images of the video. And also we will generate the text of the summaiized images.
+
+
+print("Video Summarization using Deep Learning")
